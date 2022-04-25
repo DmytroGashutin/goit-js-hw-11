e1bad2eb10b992ede5b11cb69f3bfe0f64f528ca
Dmytro Gashutin <dm.gashutin@gmail.com>
2022-04-25 21:50:13 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import './sass/main.scss';
+import NewsApiService from './api-servise';
+import markup from './murkup';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+    galleryContainer: document.querySelector('.gallery'),
+    
+};
+
+const newsApiService = new NewsApiService();
+
+
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+
+
+
+function onSearch(e) {
+    e.preventDefault();
+
+    refs.loadMoreBtn.classList.add('visually-hidden');
+    
+    newsApiService.query = e.currentTarget.elements.searchQuery.value;
+    newsApiService.resetPage();
+    newsApiService.fechArticles().then(data => {
+        clearGalleryContainer();
+        appenedHitsMarkup(data)
+        refs.loadMoreBtn.classList.remove('visually-hidden');
+        
+        console.log(newsApiService);
+
+        if (data.data.totalHits === 0) {
+            refs.loadMoreBtn.classList.add('visually-hidden');
+            Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            return;
+        }
+        
+    });
+    
+    
+    
+}
+
+function onLoadMore() {
+    newsApiService.fechArticles().then(appenedHitsMarkup);
+    
+    
+}
+
+function appenedHitsMarkup(data) {
+    refs.galleryContainer.insertAdjacentHTML('beforeend', markup(data.data.hits));
+    if (data.data.total !== 0 && data.data.hits.length === 0) {
+            refs.loadMoreBtn.classList.add('visually-hidden');
+            Notify.failure(`We're sorry, but you've reached the end of search results.`);
+            return;
+    };
+}
+
+
+function clearGalleryContainer() {
+    refs.galleryContainer.innerHTML = '';
+}
---
+++ src/murkup.js
@@ -0,0 +1,54 @@
+ 
+export default function markup(data) {
+    const markup = data.map(
+        ({ webformatURL, tags, likes, views, comments, downloads
+        }) => `
+        <div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes:${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views:${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments;${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads:${downloads}</b>
+    </p>
+  </div>
+</div>`
+    ).join('');
+    return markup;
+    
+}
+
+
+// export default function markup(data) {
+//   return data
+//     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+//       return `
+//   <div class="photo-card">
+//   <a class="gallery__item" href="${largeImageURL}">
+//     <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//     </a>
+//     <div class="info">
+//       <p class="info-item">
+//         <b>Likes: ${likes}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Views: ${views}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Comments: ${comments}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Downloads: ${downloads}</b>
+//       </p>
+//     </div>
+//   </div>`;
+//     })
+//     .join('');
+// }
